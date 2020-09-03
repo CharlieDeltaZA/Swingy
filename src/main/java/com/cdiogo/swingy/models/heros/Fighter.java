feaf2a7d008203102bc7d6af6fca5463b8ba9a6d
Calvin Diogo <calvin@diogo.co.za>
2020-09-03 11:24:12 +0200

    Add 3 new classes

---
+++ src/main/java/com/cdiogo/swingy/models/heros/Fighter.java
@@ -0,0 +1,22 @@
+package com.cdiogo.swingy.models.heros;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Fighter extends Player {
+    public Fighter(String heroName, String heroClass) {
+        super(heroName, heroClass, -69, 15, 20, 100, 3);
+        // xp = -5;
+        // hp = 100;
+        // attack = 20;
+        // defense = 15;
+        // level = 3;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
