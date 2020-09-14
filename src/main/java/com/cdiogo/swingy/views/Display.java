8b534aa635a7edfd3c2c0b37719ae25150054477
Calvin Diogo <calvin@diogo.co.za>
2020-09-14 11:51:44 +0200

    Update Interface

--- src/main/java/com/cdiogo/swingy/views/Display.java
+++ src/main/java/com/cdiogo/swingy/views/Display.java
@@ -7,7 +7,7 @@ import com.cdiogo.swingy.models.heroes.Player;
 public interface Display {
     public void startScreen();
 	public String createCharName();
-    public String createCharClass();
-	public String loadChar(List<Player> heroes);
+    public void createCharClass();
+	public void loadChar(List<Player> heroes);
     public void renderGame();
 }
