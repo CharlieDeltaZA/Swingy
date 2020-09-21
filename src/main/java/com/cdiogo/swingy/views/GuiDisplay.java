7b4a40108c37b219c65a6bb349a9ace7a3493553
Calvin Diogo <calvin@diogo.co.za>
2020-09-21 18:45:32 +0200

    Back to PC

--- src/main/java/com/cdiogo/swingy/views/GuiDisplay.java
+++ src/main/java/com/cdiogo/swingy/views/GuiDisplay.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -15,16 +16,32 @@ public class GuiDisplay implements Display {
     GameController controller;
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
-    JButton test = new JButton("Test");
-    JTextArea output = new JTextArea("Hello\n\n\n\n poes");
+    JTextArea output = new JTextArea("Hello poes", 500, 500); //??? for map display
+    JButton back = new JButton("Back");
+    JButton save = new JButton("Save");
+    JButton equip = new JButton("Equip");
+    JButton ignore = new JButton("Ignore");
+    JButton menu = new JButton("Return to Menu");
+    JButton up = new JButton("North");
+    JButton down = new JButton("South");
+    JButton right = new JButton("East");
+    JButton left = new JButton("West");
+    JButton quit = new JButton("Quit");
+    JButton create = new JButton("Create Character");
+    JButton load = new JButton("Load Character");
+    JLabel welcome = new JLabel("Welcome to SWINGY");
 
     public GuiDisplay(GameController controller) {
         this.controller = controller;
         frame.setTitle("Swingy - cdiogo");
-        frame.setSize(640, 640);
+        frame.setSize(860, 640);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        panel.add(welcome);
+        back.setBounds(20, 20, 50, 20);
+        panel.add(back);
+        // panel.add(output);
         frame.setVisible(true);
-        panel.add(test);
     }
 
     @Override
