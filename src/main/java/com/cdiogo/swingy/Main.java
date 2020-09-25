fa73791f794f8bd8020423865647c3b99c3a3558
Calvin Diogo <calvin@diogo.co.za>
2020-09-25 08:31:03 +0200

    Removed old code

--- src/main/java/com/cdiogo/swingy/Main.java
+++ src/main/java/com/cdiogo/swingy/Main.java
@@ -1,17 +1,9 @@
 package com.cdiogo.swingy;
 
 import com.cdiogo.swingy.controllers.GameController;
-// import com.cdiogo.swingy.models.heroes.Player;
-// import com.cdiogo.swingy.models.heroes.Ranger;
 
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("HelloWorld");
-        // Player ranger = new Ranger("Charlie", "Ranger");
-        // System.out.println(ranger.toString());
-        // ranger.setXp(ranger.getXp()+69);
-        // System.out.println(ranger.toString());
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         try {
             GameController controller = new GameController(args[0].toLowerCase());
             controller.playGame();
